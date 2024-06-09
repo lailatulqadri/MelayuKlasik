dea9304f5e6cbb74ecc7b4f5e581755c2d2ac804
Lailatul Qadri Zakaria <61860775+lailatulqadri@users.noreply.github.com>
2024-06-09 12:07:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import streamlit as st 
-import torch 
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import re
-from transformers import pipeline
+
 
 st.title("Welcome to the Fine-Tuned Tiny-Bert Sentiment Classifier Page")
 text = st.text_input("Please Enter Your Thoughts of The Movie: ")
