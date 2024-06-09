e1fc998c3a118d816f3c0be5432a4dc0b515d1cc
Lailatul Qadri Zakaria <61860775+lailatulqadri@users.noreply.github.com>
2024-06-09 12:04:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-import streamlit as st
-from transformers import pipeline, AutoModelForQuestionAnswering, AutoTokenizer
+import streamlit as st 
+import torch 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import re
+from transformers import pipeline
 
-@st.cache
-def load():
-    model = AutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
-    tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
-    return pipeline('question-answering', model=model, tokenizer=tokenizer)
-
-bert = load()
-
-st.title("BERT Q&A App")
-st.caption("Created using BERT model")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    with st.form("form1", clear_on_submit=False):
-        context = st.text_area("Enter your context here")
-        question = st.text_input("Enter your question here")
-        submit = st.form_submit_button("Submit", type="primary")
-
-        if submit:
-            with col2:
-                st.success('Done!')
-                st.write(bert({
-                    "question": question,
-                    "context": context
-                }))
+st.title("Welcome to the Fine-Tuned Tiny-Bert Sentiment Classifier Page")
+text = st.text_input("Please Enter Your Thoughts of The Movie: ")
