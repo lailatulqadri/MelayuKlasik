698de79fe23d54d5ae7e6f2b1a8912662e4b942b
Lailatul Qadri Zakaria <61860775+lailatulqadri@users.noreply.github.com>
2024-06-11 12:27:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import streamlit as st
-from transformers import pipeline
+#from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+
 
 # Load the sentiment analysis pipeline
 @st.cache(allow_output_mutation=True)
 def load_model():
     return pipeline("sentiment-analysis")
 
-model = load_model()
+def load_model_malay():
+    model_name = "huggingface/bert-base-bahasa-cased"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    return pipeline("text-classification", model=model, tokenizer=tokenizer)
+
+
+model = load_model_malay()
 
 # Streamlit app
 st.title("Sentiment Analysis with BERT")
